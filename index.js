cb542917fff7f791ca6aaf6d3a17544fa70ca24c
Nelson Moller <nmoller.c@gmail.com>
2019-06-15 18:05:28 -0400

    Ajouter lien vers ES6 imports.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import { CountStream }  from './src/countstream';
 
-var countStream = new CountStream('export');
+var countStream = new CountStream('book');
 import { get } from 'https';
 
-get('https://medium.com/@timoxley/named-exports-as-the-default-export-api-670b1b554f65', function(res) {
+get('https://www.manning.com', function(res) {
     res.pipe(countStream);
 });
 

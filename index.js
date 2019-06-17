dbd3bc9f008cc8fa71048e2ca2dc8a5bbed7d370
Nelson Moller <nmoller.c@gmail.com>
2019-06-17 15:41:25 -0400

    Correction des sources.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import { CountStream }  from './src/countstream';
+import {CountStream} from './src/countstream';
+import {get} from 'https';
 
-var countStream = new CountStream('book');
-import { get } from 'https';
+const countStream = new CountStream('book');
 
 get('https://www.manning.com', function(res) {
-    res.pipe(countStream);
+  res.pipe(countStream);
 });
 
 countStream.on('total', function(count) {
-    console.log('Total matches: ', count);
+  console.log('Total matches: ', count);
 });
--- src/countstream.js
+++ src/countstream.js
@@ -1,22 +1,42 @@
-import { Writable } from 'stream';
-import { inherits } from 'util';
+import {Writable} from 'stream';
+import {inherits} from 'util';
 
+/**
+ * @class CountStream
+ * @extends Writable
+ */
 export class CountStream {
-    constructor(matchText, options) {
-        Writable.call(this, options);
-        this.count = 0;
-        this.matcher = new RegExp(matchText, 'ig');
+  /**
+   *
+   * @param {string} matchText
+   * @param {Array} options
+   */
+  constructor(matchText, options) {
+    Writable.call(this, options);
+    this.count = 0;
+    this.matcher = new RegExp(matchText, 'ig');
+  }
+
+  /**
+   *
+   * @param {*} chunk
+   * @param {*} encoding
+   * @param {*} cb
+   */
+  _write(chunk, encoding, cb) {
+    const matches = chunk.toString().match(this.matcher);
+    if (matches) {
+      this.count += matches.length;
     }
-    _write(chunk, encoding, cb) {
-        var matches = chunk.toString().match(this.matcher);
-        if (matches) {
-            this.count += matches.length;
-        }
-        cb();
-    }
-    end() {
-        this.emit('total', this.count);
-    }
+    cb();
+  }
+
+  /**
+   *
+   */
+  end() {
+    this.emit('total', this.count);
+  }
 }
 
 inherits(CountStream, Writable);
